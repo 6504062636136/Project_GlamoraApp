6f6f7e275326525f138fc290aa1f7911d72e3c69
nutchaya <nutchayahw51@gmail.com>
2025-03-04 21:40:45 +0700

    A

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
         '/productlist': (context) => ProductListPage(),
         '/history': (context) => HistoryPage(), // Add this line
         '/mycart': (context) => MyCartPage(), // Add this line
-        '/mylist': (context) => MyListPage(), // Add this line
+        '/mylist': (context) => MyFavPage(), // Add this line
       },
     );
   }
--- lib/page/Register.dart
+++ lib/page/Register.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,105 +56,178 @@ class RegisterPage extends StatelessWidget {
             child: SingleChildScrollView(
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/logo2.png', // ตรวจสอบ path
-                      width: 150,
-                      height: 150,
-                    ),
-                    Text(
-                      'GLAMORA',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.deepPurple[400],
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/logo2.png', // ตรวจสอบ path
+                        width: 150,
+                        height: 150,
                       ),
-                    ),
-                    SizedBox(height: 20),
-
-                    // กล่องสีขาวที่มีฟอร์มลงทะเบียน
-                    Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.9),
-                        borderRadius: BorderRadius.circular(20),
+                      Text(
+                        'GLAMORA',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple[400],
+                        ),
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'Email',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 5),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              hintText: 'example@email.com',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none,
-                              ),
-                              filled: true,
-                              fillColor: Colors.white,
+                      SizedBox(height: 20),
+
+                      // กล่องสีขาวที่มีฟอร์มลงทะเบียน
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.9),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Name',
+                              style: TextStyle(fontWeight: FontWeight.bold),
                             ),
-                          ),
-                          SizedBox(height: 15),
-
-                          Text(
-                            'Password',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 5),
-                          TextFormField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              hintText: 'Enter your password',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide.none,
-                              ),
-                              filled: true,
-                              fillColor: Colors.white,
-                            ),
-                          ),
-                          SizedBox(height: 15),
-
-                          Row(
-                            children: <Widget>[
-                              Checkbox(
-                                value: true, // ค่าเริ่มต้น
-                                onChanged: (bool? value) {
-                                  // TODO: Implement Checkbox Logic
-                                },
-                              ),
-                              Text('Accept Terms & Conditions'),
-                            ],
-                          ),
-                          SizedBox(height: 20),
-
-                          // ปุ่ม Register
-                          SizedBox(
-                            width: double.infinity, // ปรับให้กว้างเต็ม
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.deepPurple[400],
-                                padding: EdgeInsets.symmetric(vertical: 15),
-                                textStyle: TextStyle(fontSize: 18),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30),
+                            SizedBox(height: 5),
+                            TextFormField(
+                              controller: _nameController,
+                              decoration: InputDecoration(
+                                hintText: 'Enter your name',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none,
                                 ),
+                                filled: true,
+                                fillColor: Colors.white,
                               ),
-                              onPressed: () {
-                                Navigator.pushNamed(context, '/HomePage'); // ไปหน้า HomePage
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your name';
+                                }
+                                return null;
                               },
-                              child: Text('Register', style: TextStyle(color: Colors.white)),
                             ),
-                          ),
-                        ],
+                            SizedBox(height: 15),
+
+                            Text(
+                              'Email',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 5),
+                            TextFormField(
+                              controller: _emailController,
+                              decoration: InputDecoration(
+                                hintText: 'example@email.com',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none,
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your email';
+                                }
+                                return null;
+                              },
+                            ),
+                            SizedBox(height: 15),
+
+                            Text(
+                              'Password',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 5),
+                            TextFormField(
+                              controller: _passwordController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: 'Enter your password',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none,
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your password';
+                                }
+                                return null;
+                              },
+                            ),
+                            SizedBox(height: 15),
+
+                            Text(
+                              'Confirm Password',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 5),
+                            TextFormField(
+                              controller: _confirmPasswordController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: 'Confirm your password',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none,
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please confirm your password';
+                                }
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match';
+                                }
+                                return null;
+                              },
+                            ),
+                            SizedBox(height: 15),
+
+                            Row(
+                              children: <Widget>[
+                                Checkbox(
+                                  value: true, // ค่าเริ่มต้น
+                                  onChanged: (bool? value) {
+                                    // TODO: Implement Checkbox Logic
+                                  },
+                                ),
+                                Text('Accept Terms & Conditions'),
+                              ],
+                            ),
+                            SizedBox(height: 20),
+
+                            // ปุ่ม Register
+                            SizedBox(
+                              width: double.infinity, // ปรับให้กว้างเต็ม
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.deepPurple[400],
+                                  padding: EdgeInsets.symmetric(vertical: 15),
+                                  textStyle: TextStyle(fontSize: 18),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    Navigator.pushNamed(context, '/HomePage'); // ไปหน้า HomePage
+                                  }
+                                },
+                                child: Text('Register', style: TextStyle(color: Colors.white)),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
